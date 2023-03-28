de3305e64e4b34377854586aaa1a653066acb2a7
Mateus Santos <matesantos@hotmail.com>
2023-03-28 00:31:26 -0300

    byte-banck commit inicial

---
+++ script/script.js
@@ -0,0 +1,38 @@
+const graficoDolar = document.querySelector("#graficoDolar");
+
+const graficoParaDolar = new Chart(graficoDolar, {
+  type: 'line',
+  data: {
+    labels: [],
+    datasets: [{
+      label: 'Dólar',
+      data: [],
+      borderWidth: 1
+    }]
+  },
+});
+
+setInterval(()=>conectaAPI(),5000);
+async function conectaAPI(){
+  const conecta = await fetch("https://economia.awesomeapi.com.br/json/last/USD-BRL");
+  const conectaTraduzido = await conecta.json();
+  let tempo = geraHorario();
+  let valor = conectaTraduzido.USDBRL.ask;
+  addDate(graficoParaDolar, tempo, valor);
+}
+
+function geraHorario() {
+  const data = new Date();
+  const horario = data.getHours() + ":" + data.getMinutes() + ":" + data.getSeconds();
+  return horario;
+}
+
+function addDate(grafico, legenda, dados){
+  grafico.data.labels.push(legenda);
+  grafico.data.datasets.forEach(dataset => {
+    dataset.data.push(dados);
+  });
+  grafico.update();
+}
+
+
