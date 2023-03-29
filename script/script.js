e18ec4c61b34c9ef553df257c380efd93ce3c2b2
Mateus Santos <matesantos@hotmail.com>
2023-03-28 23:23:38 -0300

    criação da pasta workers

--- script/script.js
+++ script/script.js
@@ -1,3 +1,5 @@
+import printCotacao from "./printCotacao.js";
+
 const graficoDolar = document.querySelector("#graficoDolar");
 
 const graficoParaDolar = new Chart(graficoDolar, {
@@ -16,9 +18,10 @@ setInterval(()=>conectaAPI(),5000);
 async function conectaAPI(){
   const conecta = await fetch("https://economia.awesomeapi.com.br/json/last/USD-BRL");
   const conectaTraduzido = await conecta.json();
-  let tempo = geraHorario();
-  let valor = conectaTraduzido.USDBRL.ask;
+  const tempo = geraHorario();
+  const valor = conectaTraduzido.USDBRL.ask;
   addDate(graficoParaDolar, tempo, valor);
+  printCotacao("dolar", valor);
 }
 
 function geraHorario() {
